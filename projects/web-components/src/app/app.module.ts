4c1a57529c3b4a3d9d46b6eeb2f9f6ee140b9a70
ashish <93189+ashishshukla1681@users.noreply.github.com>
2020-12-26 00:30:01 -0500

    New component created, element library added & Removeed the declaration of AppComponent, also removeed the app.component.ts and related HTML, CSS and spec.ts file and it's import.

--- projects/web-components/src/app/app.module.ts
+++ projects/web-components/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { MyElementComponent } from './my-element/my-element.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MyElementComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [MyElementComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private injector: Injector) { }
+  ngDoBootstrap() {
+    const el = createCustomElement(MyElementComponent, { injector: this.injector });
+    customElements.define('my-element', el);
+  }
+}
