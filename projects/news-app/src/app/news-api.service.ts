493450c55ce452aa3a5cd0c34b40e270e31455f3
ashish <93189+ashishshukla1681@users.noreply.github.com>
2020-12-27 00:47:56 -0500

    News api service provider created, css, global styles added etc.

---
+++ projects/news-app/src/app/news-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = '263eb0ad47ab404b907a64e0e0430806';
+
+  constructor(private http:HttpClient) { }
+  initSources(){
+     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesByID(source: String){
+   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+} 
